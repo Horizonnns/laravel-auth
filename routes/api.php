2a2d0b78ab5f3dc817b2a0bad9518b3d55e150ac
Horizonnns <for.office.gthb@gmail.com>
2023-12-29 11:35:29 +0500

    Removed unused post

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ Route::post('register', [\App\Http\Controllers\AuthController::class, 'register'
 Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
 
 
-// Route::post('createUserWithRoles', [\App\Http\Controllers\AuthController::class, 'createUserWithRoles']);
-
-
-
 Route::middleware('auth:sanctum')->group(function() {
 Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
 Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
